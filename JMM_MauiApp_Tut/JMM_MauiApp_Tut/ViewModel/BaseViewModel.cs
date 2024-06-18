c35ba540f9aa82913dd396a56940c966b770e48e
Israel Palma <isra.palma@gmail.com>
2024-06-17 21:13:32 -0600

    In this commit we added the Monkey model and the BaseViewModel for demo purposes, James is explaining how we can implement our own ViewModels using INotifyPropertyChanged interface by iheriting from it. Next we'll see how to use other techniques, such as using the C# compiler anotations and the MVVM Toolkit

---
+++ JMM_MauiApp_Tut/JMM_MauiApp_Tut/ViewModel/BaseViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+
+namespace JMM_MauiApp_Tut.ViewModel;
+public class BaseViewModel : INotifyPropertyChanged
+{
+    private bool isBusy;
+    private string title;
+
+    public bool IsBusy
+    {
+        get => isBusy;
+        set
+        {
+            if(isBusy == value) return;
+
+            isBusy = value;
+            OnPropertyChanged(nameof(IsBusy));
+            //OnPropertyChanged("IsBusy");
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public void OnPropertyChanged(string name)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
+
