858cf9b373a59eb5c1fda72ff6552ac7adfe21d5
Israel Palma <isra.palma@gmail.com>
2024-06-17 21:32:54 -0600

    We just saw how to use the MVVM Community Toolkit anotation to automatically source generate everything we need for our ViewModel class witout having to write all the boilerplate code

--- JMM_MauiApp_Tut/JMM_MauiApp_Tut/ViewModel/BaseViewModel.cs
+++ JMM_MauiApp_Tut/JMM_MauiApp_Tut/ViewModel/BaseViewModel.cs
@@ -1,47 +1,16 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace JMM_MauiApp_Tut.ViewModel;
-public class BaseViewModel : INotifyPropertyChanged
+[INotifyPropertyChanged]
+public partial class BaseViewModel
 {
+
+    public BaseViewModel() { }
     private bool isBusy;
     private string title;
 
-    public bool IsBusy
-    {
-        get => isBusy;
-        set
-        {
-            if(isBusy == value) return;
-
-            isBusy = value;
-            //We can call OnPropertyChanged() witout passing any params
-            //thanks to annotation in line 26 [CallerMemberName]
-            OnPropertyChanged();
-            //OnPropertyChanged(nameof(IsBusy));
-            //OnPropertyChanged("IsBusy");
-        }
-    }
     
-    public bool IsNotBusy => !isBusy;
-
-    public string Title
-    {
-        get => title;
-        set
-        {
-            if (title == value) return;
-            title = value;
-            OnPropertyChanged();
-            OnPropertyChanged(nameof(IsNotBusy));
-        }
-    }
-
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-    public void OnPropertyChanged([CallerMemberName]string? name =null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-    }
 }
 
